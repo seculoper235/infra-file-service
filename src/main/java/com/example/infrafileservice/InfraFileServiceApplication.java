892026cd3df3760760714674634dc0b6e43063be
seculoper <mir8725@naver.com>
2025-03-06 19:34:00 +0900

    Initial commit

---
+++ src/main/java/com/example/infrafileservice/InfraFileServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.infrafileservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfraFileServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfraFileServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/infrafileservice/InfraFileServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.infrafileservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfraFileServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
